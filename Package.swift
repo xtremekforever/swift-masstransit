aabdedcd121370145c3cd3ba233ef48322c9897f
Jesse L. Zamora <xtremekforever@gmail.com>
2024-11-13 14:23:54 -0500

    Update swift-rabbitmq dependency and minimum version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["MassTransit"])
     ],
     dependencies: [
-        .package(url: "https://github.com/xtremekforever/swift-rabbitmq", branch: "main"),
+        .package(url: "https://github.com/xtremekforever/swift-rabbitmq", from: "0.1.0"),
         .package(url: "https://github.com/apple/swift-distributed-tracing-extras.git", from: "1.0.0-beta.1"),
     ],
     targets: [
